2a84dd1119ac89f715a80d5857682abbbe57e509
Tom Herrmann <tom_herrmannhhoutlook.de>
2020-05-23 12:44:15 +0200

    Issue #2 - Random; Added some functionality

---
+++ Sampling/Random.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>	
+#include <stdio.h>	
+#include <time.h>
+
+int getRandomNumbers(unsigned int n)
+{
+	int i = 0;
+
+	srand48(time(NULL)); 
+	while(i < n)
+	{
+		printf("%f\n",drand48());
+		i++;
+	}
+	return(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[])
+{
+	int n;
+	if(argc != 2 || (	sscanf(argv[1], "%u", &n)) != 1)	
+  	{	
+  		fprintf(stderr, "Not a valid Argument");	
+  		exit(EXIT_FAILURE);	
+  	}
+  	getRandomNumbers(n);
+  }
