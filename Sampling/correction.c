f871cd8349199206fec468e1d3824030d57de7d5
RalfSchmelcher <65372609+RalfSchmelcher@users.noreply.github.com>
2020-05-23 19:18:54 +0200

    Add files via upload
    Issue #1 - correction; application of sensitivity and specificity to the fraction of subjects tested positive.

---
+++ Sampling/correction.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define SPECIFICITY 0.991
+#define SENSITIVITY 0.909
+
+//applies correction to lessen bias
+double correct(double f)
+{
+  f = (f + SPECIFICITY - 1) / (SPECIFICITY + SENSITIVITY - 1);
+  return f;
+}
+
+//input: f = n_measured / n_subjects --> 0 < f < 1
+int main(int argc, char *argv[])
+{
+  double f;
+  switch (argc)
+  {
+    case 2:
+      if((sscanf(argv[1], "%lf", &f) == 1) && (argv[1] < 1) && (argv[1] > 0))
+      {
+        correct(f);
+        break;
+      }
+    default:
+      fprintf(stderr, "Usage: %s <fraction of positive tests>", argv[1]);
+      return 1;
+  }
+  printf("successfully applied correction!\n");
+  return 0;
+}
