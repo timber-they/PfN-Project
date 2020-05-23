3c08487af6e4016693dd63a34b3cb59047316c50
Tom Herrmann <tom_herrmannhhoutlook.de>
2020-05-23 12:48:09 +0200

    Issed #4 - Random; added Comments

--- Sampling/Random.c
+++ Sampling/Random.c
@@ -6,8 +6,8 @@ int getRandomNumbers(unsigned int n)
 {
 	int i = 0;
 
-	srand48(time(NULL)); 
-	while(i < n)
+	srand48(time(NULL)); //Seed Generation based on system Time
+	while(i < n) //printing out some values
 	{
 		printf("%f\n",drand48());
 		i++;
@@ -18,7 +18,7 @@ int getRandomNumbers(unsigned int n)
 int main(int argc, char *argv[])
 {
 	int n;
-	if(argc != 2 || (	sscanf(argv[1], "%u", &n)) != 1)	
+	if(argc != 2 || (sscanf(argv[1], "%u", &n)) != 1)	
   	{	
   		fprintf(stderr, "Not a valid Argument");	
   		exit(EXIT_FAILURE);	
