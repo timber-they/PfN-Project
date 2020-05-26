ec0863c8de5e3ed53978ed3a536b55f0e341113f
s-hartig <steffen.hartig@studium.uni-hamburg.de>
2020-05-26 13:05:59 +0200

    Issue #5 - Add medium function

---
+++ Extraction/Medium.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+double medium(double* sample, size_t n_elems)
+{
+  int i;
+  double sum=0, medium;
+
+  for(i=0; i<n_elems; i++)
+  {
+    sum+=sample[i];
+  }
+  medium = sum/n_elems;
+
+  return medium;
+}
+
+int main(void)
+{
+  double sample[] = {1,2,3,4,5,6,7,8,9,10,11};
+  size_t len = sizeof(sample) / sizeof(double);
+  double avg = medium(sample,len);
+  printf("avg: %lf\n",avg);
+
+  return EXIT_SUCCESS;
+}
