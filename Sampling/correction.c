4c0eb8c0023f293252dff961aa601cbfcbbee266
RalfSchmelcher <65372609+RalfSchmelcher@users.noreply.github.com>
2020-05-27 20:47:12 +0200

    Issue #13 - Applying specifity and selectivity; updated correction.c for format convention
    Updated correction.c for format convention (space after // and tab-size of 4).

--- Sampling/correction.c
+++ Sampling/correction.c
@@ -4,29 +4,29 @@
 #define SPECIFICITY 0.991
 #define SENSITIVITY 0.909
 
-//applies correction to lessen bias
+// applies correction to lessen bias
 double correct(double f)
 {
-  f = (f + SPECIFICITY - 1) / (SPECIFICITY + SENSITIVITY - 1);
-  return f;
+    f = (f + SPECIFICITY - 1) / (SPECIFICITY + SENSITIVITY - 1);
+    return f;
 }
 
-//input: f = n_measured / n_subjects --> 0 < f < 1
+// input: f = n_measured / n_subjects --> 0 < f < 1
 int main(int argc, char *argv[])
 {
-  double f;
-  switch (argc)
-  {
-    case 2:
-      if((sscanf(argv[1], "%lf", &f) == 1) && (argv[1] < 1) && (argv[1] > 0))
-      {
-        correct(f);
-        break;
-      }
-    default:
-      fprintf(stderr, "Usage: %s <fraction of positive tests>", argv[0]);
-      return EXIT_FAILURE;
-  }
-  printf("successfully applied correction!\n");
-  return EXIT_SUCCESS;
+    double f;
+    switch (argc)
+    {
+        case 2:
+          if((sscanf(argv[1], "%lf", &f) == 1) && (argv[1] < 1) && (argv[1] > 0))
+          {
+            correct(f);
+            break;
+          }
+        default:
+          fprintf(stderr, "Usage: %s <fraction of positive tests>", argv[0]);
+          return EXIT_FAILURE;
+    }
+    printf("successfully applied correction!\n");
+    return EXIT_SUCCESS;
 }
