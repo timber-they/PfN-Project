7ef91c5d95e7f1bd9fceae44df26ff84ea382fc9
Julius Bittner <8bittner@informatik.uni-hamburg.de>
2020-05-29 02:36:30 +0200

    Issue #7 - Confidence intervals; made it a real and working function, added Makefile

--- Extraction/ConfidenceIntervals.c
+++ Extraction/ConfidenceIntervals.c
@@ -6,12 +6,12 @@
 #include <stdlib.h>
 #include <math.h>
 
-// conf: calculate confidence interval of given numbers with given level
+// gaussConf: calculate confidence interval of some data with given standard deviation, number of values and level assuming gauss distribution
 // give: standard deviation
 // give: number of values as unsigned int
 // give: confidence level to use. Choose from 0.8, 0.85, 0.9, 0.95, 0.99, 0.995, 0.999
 // return: confidence as deviation from medium, to the upside as well as to the downside!
-double conf(double standDev, unsigned int numOfVals, float level)
+double gaussConf(double standDev, unsigned int numOfVals, float level)
 {
     float Z;
     unsigned int Level = level * 1000;
@@ -34,8 +34,40 @@ double conf(double standDev, unsigned int numOfVals, float level)
     return c;
 }
 
-int main(int argc,char *argv[])
+// conf: calculate confidence interval of given data with given level
+// give: data as sorted array of doubles
+// give: desired confidence level in (0.0, 1.0]
+// return: 2-array with minimum and maximum of confidence interval
+double * conf(double *sorted, size_t n_elems, float level)
 {
-    printf("%f\n", conf(20, 40, 0.95));
-    return EXIT_SUCCESS;
+	if (n_elems == 0)
+	{
+		fprintf(stderr, "Please input array of at least length 1");
+	}
+	if (level <= 0 || level > 1)
+	{
+		fprintf(stderr, "Please input level greater than 0 and 1 at maximum");
+	}
+	
+	static double intervals[2];
+	int min, max;
+	max = (int) ((0.5 + level / 2) * n_elems - 1);
+	min = (int) ((0.5 - level / 2) * n_elems);
+
+	intervals[0] = sorted[min];
+	intervals[1] = sorted[max];
+	
+	return intervals;
 }
+
+int main()
+{
+	double vals[] = { 1, 4, 4, 5, 9, 10, 11, 11, 12, 14, 14, 17, 18, 19, 21, 25, 25, 25, 30, 31, 38, 40, 45, 49, 90 };
+	size_t size = sizeof vals/sizeof vals[0];
+	float level = 0.8;
+
+	double* c;
+	c = conf(vals, size, level);
+
+	printf("%f of %li values lies in [%f, %f]\n", level, size, c[0], c[1]);
+}   
