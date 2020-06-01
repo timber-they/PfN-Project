8eccd7a29ae6b6f7b5f0e7c33ce441d68f914054
s-hartig <steffen.hartig@studium.uni-hamburg.de>
2020-06-01 15:37:26 +0200

    Medium - fix spaces

--- Extraction/Medium.c
+++ Extraction/Medium.c
@@ -3,24 +3,24 @@
 
 double medium(double* sample, size_t n_elems)
 {
-  int i;
-  double sum=0, medium;
+    int i;
+    double sum=0, medium;
 
-  for(i=0; i<n_elems; i++)
-  {
-    sum+=sample[i];
-  }
-  medium = sum/n_elems;
+    for(i=0; i<n_elems; i++)
+    {
+        sum+=sample[i];
+    }
+    medium = sum/n_elems;
 
-  return medium;
+    return medium;
 }
 
 int main(void)
 {
-  double sample[] = {1,2,3,4,5,6,7,8,9,10,11};
-  size_t len = sizeof(sample) / sizeof(double);
-  double avg = medium(sample,len);
-  printf("avg: %lf\n",avg);
+    double sample[] = {1,2,3,4,5,6,7,8,9,10,11};
+    size_t len = sizeof(sample) / sizeof(double);
+    double avg = medium(sample,len);
+    printf("avg: %lf\n",avg);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
