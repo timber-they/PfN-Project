64018369d938ab42772fcb5fb17ebf0e5d0c9b89
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2020-06-01 18:06:19 +0200

    Issue #2 - Random; replaced tabs

--- Sampling/Random.c
+++ Sampling/Random.c
@@ -4,46 +4,46 @@
 
 int getRandomNumbers(unsigned int n)
 {
-	int i = 0;
-	int seed = 100;
-	srand48(seed); // Seed Generation seed variable
-	while(i <= n) // printing out some values
-	{
-		printf("%f\n",drand48());
-		i++;
-	}
-	return(EXIT_SUCCESS);
+    int i = 0;
+    int seed = 100;
+    srand48(seed); // Seed Generation seed variable
+    while(i <= n) // printing out some values
+    {
+        printf("%f\n",drand48());
+        i++;
+    }
+    return(EXIT_SUCCESS);
 }
 
 void intoArray(unsigned int n)
 {
-	//only usable for low n
-	int seed = 100;
-	double arr[n];
-	int idx = 0;
-	srand48(seed);
-	while(idx <= n)
-	{
-			arr[idx] = drand48();
-			printf("%f\n",arr[idx]);
-			idx++;
-	}
+    //only usable for low n
+    int seed = 100;
+    double arr[n];
+    int idx = 0;
+    srand48(seed);
+    while(idx <= n)
+    {
+            arr[idx] = drand48();
+            printf("%f\n",arr[idx]);
+            idx++;
+    }
 }
 
 void getRandomNumber(unsigned int n)
 {
-	srand48(time(NULL)); // Seed Generation based on seed variable
-	printf("%f\n",drand48());
+    srand48(time(NULL)); // Seed Generation based on seed variable
+    printf("%f\n",drand48());
 }
 
 
 int main(int argc, char *argv[])
 {
-	int n;
-	if(argc != 2 || (sscanf(argv[1], "%u", &n)) != 1)	
-  	{	
-  		fprintf(stderr, "Not a valid Argument");	
-  		exit(EXIT_FAILURE);	
-  	}
-  	intoArray(n);
+    int n;
+    if(argc != 2 || (sscanf(argv[1], "%u", &n)) != 1)	
+      {	
+          fprintf(stderr, "Not a valid Argument");	
+          exit(EXIT_FAILURE);	
+      }
+      intoArray(n);
   }
