083fa781dfbeb822dc9576c3a078be4cd44be66f
Pascal Klinger <18pklinge@informatik.uni-hamburg.de>
2020-06-04 13:18:31 +0200

    Issue #11 - Painting histograms; Mistake in .h

--- Extraction/Histograms.h
+++ Extraction/Histograms.h
@@ -2,6 +2,6 @@
 #define HISTOGRAMS
 
 //paints a histogram based on source data. the maximum lenth for the filname of the sources is 32 characters
-int paintHistogram(char *source)
+int paintHistogram(char *source);
 
 #endif
