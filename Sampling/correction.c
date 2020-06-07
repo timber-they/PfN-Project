1f4479b1424332d698f59567b85bbb750af8d7c6
RalfSchmelcher <65372609+RalfSchmelcher@users.noreply.github.com>
2020-06-07 22:38:19 +0200

    Issue #13 - Applying specifity and selectivity; updated correction.c
    Issue #13 - Applying specifity and selectivity; updated correction.c
    removed unnecessary main-method and made changes accordingly.

--- Sampling/correction.c
+++ Sampling/correction.c
@@ -5,28 +5,17 @@
 #define SENSITIVITY 0.909
 
 // applies correction to lessen bias
-double correct(double f)
+double correction(double f)
 {
+  // input: f = n_measured / n_subjects --> 0 <= f <= 1
+  if ((f <= 1) && (f >= 0))
+  {
     f = (f + SPECIFICITY - 1) / (SPECIFICITY + SENSITIVITY - 1);
     return f;
-}
-
-// input: f = n_measured / n_subjects --> 0 < f < 1
-int main(int argc, char *argv[])
-{
-    double f;
-    switch (argc)
-    {
-        case 2:
-          if((sscanf(argv[1], "%lf", &f) == 1) && (argv[1] < 1) && (argv[1] > 0))
-          {
-            correct(f);
-            break;
-          }
-        default:
-          fprintf(stderr, "Usage: %s <fraction of positive tests>", argv[0]);
-          return EXIT_FAILURE;
-    }
-    printf("successfully applied correction!\n");
-    return EXIT_SUCCESS;
+  }
+  else
+  {
+    fprintf(stderr, "correction: input f has to obey 0 <= f <= 1\n");
+    return f;
+  }
 }
