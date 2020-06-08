ee1c729e38087f16e1d83c152c4dd425e08cea90
metacolon <timo@teemze.de>
2020-06-08 09:15:45 +0200

    Issue #13 - Applying specificity and selectivity; Properly allow tolerances

--- Tests/Check_correction.h
+++ Tests/Check_correction.h
@@ -4,6 +4,9 @@
 START_TEST(test_correction)
 {
   double f1 = correction(0.9);
-  ck_assert(abs(f1 - 0.99) < 0.000001);
+  double diff = f1 - 0.99;
+  if (diff < 0)
+      diff = -diff;
+  ck_assert(diff < 0.0000001);
 }
 END_TEST
