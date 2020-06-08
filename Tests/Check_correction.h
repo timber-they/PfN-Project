0ea21202318d04e52dd3c0e9636bc775e69aa9b6
metacolon <timo@teemze.de>
2020-06-08 09:13:16 +0200

    Issue #13 - Applying specificity and selectivity; Use older check library functions and allow tolerance

--- Tests/Check_correction.h
+++ Tests/Check_correction.h
@@ -1,8 +1,9 @@
 #include "../Sampling/correction.h"
+#include <stdlib.h>
 
 START_TEST(test_correction)
 {
   double f1 = correction(0.9);
-  ck_assert_double_eq(f1, 0.99);
+  ck_assert(abs(f1 - 0.99) < 0.000001);
 }
 END_TEST
