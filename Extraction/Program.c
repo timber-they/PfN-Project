217724aeb0c4118816f2acbfb30d3b2f38d44250
Hannes Neuschmidt <8neuschm@informatik.uni-hamburg.de>
2020-06-08 21:22:14 +0200

    Added main dummy main function for Extraction
    If the body is commented out the program will NOT compile.
    The makefile needs to be changed so all the files are linked correctly

---
+++ Extraction/Program.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "CSV_Output.h"
+#include "ConfidenceIntervals.h"
+#include "Histograms.h"
+#include "Median.h"
+/* #include "Medium.h" */ // This file is still missing
+
+// THIS IS ONLY A DUMMY IMPLEMENTATION to see if the program compiles
+int main(void)
+{
+  /* (void) createCSV("Hallo", "Welt"); */
+  /* double array[1] = {0, }; */
+  /* (void) conf_itvl(array, 1, 0.8); */
+  /* (void) paintHistogram("Hallo"); */
+  /* (void) median_sort(array, 1); */
+  return EXIT_SUCCESS;
+}
