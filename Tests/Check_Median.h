a223133305f3bb94717abf16d646dd94b54a672e
Hannes Neuschmidt <8neuschm@informatik.uni-hamburg.de>
2020-06-08 16:27:00 +0200

    removed modern features from Check_Median.h

--- Tests/Check_Median.h
+++ Tests/Check_Median.h
@@ -5,7 +5,7 @@ START_TEST(test_median_sorted) {
   double even_sorted[4] = {1.0, 2.0, 3.0, 4.0};     // median is 2.5
 
   double test_odd_sorted = median_sort(odd_sorted, 5);
-  ck_assert_double_eq(test_odd_sorted, 4.0);
+  ck_assert(test_odd_sorted == 4.0);
   double test_even_sorted = median_sort(even_sorted, 4);
   double diff = test_even_sorted - 2.5;
   ck_assert(diff<0.1 && diff> - 0.1);
@@ -17,7 +17,7 @@ START_TEST(test_median_unsorted) {
   double even_unsorted[4] = {4.0, 2.0, 3.0, 1.0};     // median is 2.5
 
   double test_odd_unsorted = median_sort(odd_unsorted, 5);
-  ck_assert_double_eq(test_odd_unsorted, 4.0);
+  ck_assert(test_odd_unsorted == 4.0);
   double test_even_unsorted = median_sort(even_unsorted, 4);
   double diff = test_even_unsorted - 2.5;
   ck_assert(diff < 0.1 && diff > -0.1);
