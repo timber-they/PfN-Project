55e9db731cd8bc897be64706557a23a404550aba
s-hartig <steffen.hartig@studium.uni-hamburg.de>
2020-06-09 12:30:37 +0200

    Improve medium test

--- Tests/Check_Medium.h
+++ Tests/Check_Medium.h
@@ -2,12 +2,20 @@
 
 START_TEST(test_medium)
 {
-    double sample[] = {1,2,3};
-    size_t len = sizeof(sample) / sizeof(double);
-    ck_assert(medium(sample,len)==2.0);
-    sample[0] = 2;
-    sample[1] = 1;
-    sample[2] = 3;
-    ck_assert(medium(sample,len)==2.0);
+    int i,j;
+    double sample[] = {1.0,2.0,3.0,4.0,5.0};
+    double temp, med = 3.0;
+    size_t len = sizeof(sample) / sizeof(sample[0]);
+
+    for(i = 0; i<len; i++)
+    {
+        temp = sample[len-1];
+        for (j = len-1; j > 0; j--)
+        {
+            sample[j] = sample[j-1];
+        }
+        sample[0]=temp;
+        ck_assert(medium(sample,len)==med);
+    }
 }
 END_TEST
