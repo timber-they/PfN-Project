cbea13e91b740c45f70f4eab2345676d62be81b4
Julius Bittner <8bittner@informatik.uni-hamburg.de>
2020-06-11 15:50:11 +0200

    Issue #21 - Concluding extraction; got all TODOs and some functionality together

--- Extraction/Program.c
+++ Extraction/Program.c
@@ -4,27 +4,27 @@
 #include "Histograms.h"
 #include "Median.h"
 #include "Medium.h"
-#define CONFIDENCE_LEVEL 0.95
 
-/* Input an array of trial results, which contains the number of infected
- * people given by the sampling, the length of the array and the size of the
- * sampled population.
- * Returns some extracted statistics.
+/* Input a file name containing trial results data, int with number of trials
+ * (should be number of lines of trial results file), int basic population size
+ * and double confidence level.
+ * Writes statistics into csv for gnuplot.
  */
 int main(int argc, char *argv[])
 {
-    unsigned int *trial results, population_size, data_median,
+    unsigned int *trial_results, population_size, data_median,
                  *confidence_interval;
     size_t number_of_trials;
-    double data_medium, *relative_results, **diagram_data;
+    double data_medium, confidence_level, *relative_results, **diagram_data;
     
     /* diagram data: array of 2-arrays containing diagram point {x,y}
      * x-axis: n_infected (= trial_results)
      * y-axis: n_simulations (n_infected) / N_simulations
      */
     
-    // TODO check file for format failures
-    // TODO read trial_results from file
+    // TODO Julius check file for format failures
+    // TODO Julius parse start parameters
+    // TODO Julius read trial_results from file
     
     data_median = median_sort(trial_results, number_of_trials);
     data_medium = medium(trial_results, number_of_trials);
@@ -36,9 +36,10 @@ int main(int argc, char *argv[])
         relative_results[i] = (double) trial_results[i] / population_size;
     }
     
-    // TODO accumulate histogram intervals?
-    // TODO write data into gnuplot-readable (csv) file
-    // TODO paint gnuplot data
+    // TODO Hannes count trial_results to get n_simulations > extract diagram data
+    // TODO Hannes accumulate histogram intervals?
+    // TODO Pascal write data into gnuplot-readable (csv) file
+    // TODO Pascal paint gnuplot data
     
     return EXIT_SUCCESS;
 }
