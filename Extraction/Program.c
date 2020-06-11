4801b6c4db56c9fe7e45f2dcbbd04342cdaf188c
Julius Bittner <8bittner@informatik.uni-hamburg.de>
2020-06-11 15:33:58 +0200

    Issue #21 - Concluding extraction; added some skeleton

--- Extraction/Program.c
+++ Extraction/Program.c
@@ -3,15 +3,43 @@
 #include "ConfidenceIntervals.h"
 #include "Histograms.h"
 #include "Median.h"
-/* #include "Medium.h" */ // This file is still missing
+#include "Medium.h"
+#define CONFIDENCE_LEVEL 0.95
 
-// THIS IS ONLY A DUMMY IMPLEMENTATION to see if the program compiles
-int main(void)
+/* Input an array of trial results, which contains the number of infected
+ * people given by the sampling, the length of the array and the size of the
+ * sampled population.
+ * Returns some extracted statistics.
+ */
+int main(int argc, char *argv[])
 {
-  /* (void) createCSV("Hallo", "Welt"); */
-  /* double array[1] = {0, }; */
-  /* (void) conf_itvl(array, 1, 0.8); */
-  /* (void) paintHistogram("Hallo"); */
-  /* (void) median_sort(array, 1); */
-  return EXIT_SUCCESS;
+    unsigned int *trial results, population_size, data_median,
+                 *confidence_interval;
+    size_t number_of_trials;
+    double data_medium, *relative_results, **diagram_data;
+    
+    /* diagram data: array of 2-arrays containing diagram point {x,y}
+     * x-axis: n_infected (= trial_results)
+     * y-axis: n_simulations (n_infected) / N_simulations
+     */
+    
+    // TODO check file for format failures
+    // TODO read trial_results from file
+    
+    data_median = median_sort(trial_results, number_of_trials);
+    data_medium = medium(trial_results, number_of_trials);
+    confidence_interval = conf_itvl (trial_results, number_of_trials,
+                                     CONFIDENCE_LEVEL);
+
+    for (int i = 0; i < number_of_trials; i++)
+    {
+        relative_results[i] = (double) trial_results[i] / population_size;
+    }
+    
+    // TODO accumulate histogram intervals?
+    // TODO write data into gnuplot-readable (csv) file
+    // TODO paint gnuplot data
+    
+    return EXIT_SUCCESS;
 }
+
