f5c22bd2091d81aa92832098ba910a4dcb10485d
RalfSchmelcher <65372609+RalfSchmelcher@users.noreply.github.com>
2020-06-12 16:08:45 +0200

    reverted changes

--- Tests/Check_correction.h
+++ Tests/Check_correction.h
@@ -1,20 +1,12 @@
-#include "../Sampling/double_with_error.h"
 #include "../Sampling/correction.h"
 #include <stdlib.h>
-
 START_TEST(test_correction)
 {
-    double_with_error f1, f2;
-    f1 = {0.9, EXIT_SUCCESS};
-    f1 = {0.1, EXIT_SUCCESS};
-    f1 = correction(f1);
-    f2 = correction(f2);
-    double diff1 = f1->d - 0.99, diff2 = f2->d - 0.10111111;
-    if (diff1 < 0)
-        diff1 = -diff1;
-    if (diff2 < 0)
-        diff2 = -diff2;
-    ck_assert(diff1 < 0.0000001);
-    ck_assert(diff2 < 0.0000001);
+  double f1 = correction(0.9);
+  ck_assert(abs(f1 - 0.99) < 0.000001);
+  double diff = f1 - 0.99;
+  if (diff < 0)
+      diff = -diff;
+  ck_assert(diff < 0.0000001);
 }
 END_TEST
