067771f4dbbad20152be2659152bbd11318c89d3
RalfSchmelcher <65372609+RalfSchmelcher@users.noreply.github.com>
2020-06-12 15:59:57 +0200

    Issue #13 - Applying specificity and selectivity; second test and uses structure
     Issue #13 - Applying specificity and selectivity; second test and uses structure
    the test has a second assertion and now uses the double_with_error structure

--- Tests/Check_correction.h
+++ Tests/Check_correction.h
@@ -1,12 +1,22 @@
+#include "../Sampling/double_with_error.h"
 #include "../Sampling/correction.h"
 #include <stdlib.h>
 
 START_TEST(test_correction)
 {
-  double f1 = correction(0.9);
-  double diff = f1 - 0.99;
-  if (diff < 0)
-      diff = -diff;
-  ck_assert(diff < 0.0000001);
+    double_with_error f1, f2;
+    f1->d = 0.9;
+    f1->err = EXIT_SUCCESS;
+    f2->d = 0.1;
+    f2->err = EXIT_SUCCESS;
+    f1 = correction(f1);
+    f2 = correction(f2);
+    double diff1 = f1->d - 0.99, diff2 = f2->d - 0.10111111;
+    if (diff1 < 0)
+        diff1 = -diff1;
+    if (diff2 < 0)
+        diff2 = -diff2;
+    ck_assert(diff1 < 0.0000001);
+    ck_assert(diff2 < 0.0000001);
 }
 END_TEST
