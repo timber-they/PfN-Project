260955789f27fd57ca8f203215f1d47e0fa0c0b9
RalfSchmelcher <65372609+RalfSchmelcher@users.noreply.github.com>
2020-06-12 16:14:35 +0200

    reverted changes

--- Tests/Check_correction.h
+++ Tests/Check_correction.h
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 START_TEST(test_correction)
 {
-  double f1 = correction(0.9);
-  ck_assert(abs(f1 - 0.99) < 0.000001);
-  double diff = f1 - 0.99;
-  if (diff < 0)
-      diff = -diff;
-  ck_assert(diff < 0.0000001);
+    double f1 = correction(0.9);
+    double diff = f1 - 0.99;
+    if (diff < 0)
+        diff = -diff;
+    ck_assert(diff < 0.0000001);
 }
 END_TEST
