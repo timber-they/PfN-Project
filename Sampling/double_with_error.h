1b67ad58e7abe482feeed94b610ec762d862a413
RalfSchmelcher <65372609+RalfSchmelcher@users.noreply.github.com>
2020-06-12 15:55:07 +0200

    Issue #13 - Applying specifity and selectivity; added double_with_error.h
    Issue #13 - Applying specifity and selectivity; added double_with_error.h
    Adds a structure for storing a double with an error-flag.

---
+++ Sampling/double_with_error.h
@@ -0,0 +1,12 @@
+#ifndef DOUBLE_WITH_ERROR
+#define DOUBLE_WITH_ERROR
+
+// A structure for storing a double wih a possible error
+typedef struct {
+    // The double value
+    double d;
+    // The error
+    int err;
+} double_with_error;
+
+#endif
