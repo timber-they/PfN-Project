52494c25c512ae4e31fe50d2e178205b8afef343
MetaColon <timo@teemze.de>
2020-06-13 17:30:16 +0200

    Corrected random test

--- Tests/Check_Random.h
+++ Tests/Check_Random.h
@@ -5,6 +5,7 @@ START_TEST(test_random)
     int seed = 100;
 	initRandom(seed);
 	float test1 = getRandomNumber();
+	initRandom(seed);
 	float test2 = getRandomNumber();
 	ck_assert(test1 == test2);
 }
