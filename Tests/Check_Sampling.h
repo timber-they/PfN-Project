c6c873987ebcc20dfd54901a2f348dffa9b3a929
Tore von Schwartz <8schwart@informatik.uni-hamburg.de>
2020-06-17 18:39:22 +0200

    Issue #18 - Integration Test; added possibilty to create multiple samples

--- Tests/Check_Sampling.h
+++ Tests/Check_Sampling.h
@@ -1,21 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define PARAMETERS "0.3 1000000 10000"
-#define PROPABILITY 0.3
+#define PARAMETERS " 0.3 1000000 1 10000"
 #define SAMPLESIZE 10000
 
 
 START_TEST(test_sampling)
 {
     FILE *resultFile = NULL;
-    const char *resultFileName = "../Sampling/sampleResult.txt";
+    const char *resultFileName = "../Sampling/sampleResult.tsv";
     int return_code;
-    // maximum of 10 chars
-    char *resultChar = malloc(sizeof(char) * 10);
+    // maximum of 20 chars
+    char *resultChar = malloc(sizeof(char) * 20);
+    char *seperator = "\t";
     int result = -1;
+    char *sizeptr;
     
-    return_code = system("cd .. && cd Sampling && make && ./Program.x "PARAMETERS);
+    return_code = system("cd .. && cd Sampling && make && ./Program.x"PARAMETERS);
 
     if (return_code != 0)
     {
@@ -29,16 +30,18 @@ START_TEST(test_sampling)
     }
     else
     {
-        if (fgets(resultChar, 10, resultFile) == NULL)
+        if (fgets(resultChar, 20, resultFile) == NULL)
         {
             ck_abort_msg("Reading from the file failed");
         }
-        result = atoi(resultChar);
+        sizeptr = strtok(resultChar, seperator);
+        sizeptr = strtok(NULL, seperator);
+        result = atoi(sizeptr);
     }
     
     // Check for interval +- 10 around the expected value %
-    ck_assert(result > (int) (SAMPLESIZE * (PROPABILITY - 0.1)));
-    ck_assert(result < (int) (SAMPLESIZE * (PROPABILITY + 0.1)));
+    ck_assert(result > (int) (SAMPLESIZE * 0.212));
+    ck_assert(result < (int) (SAMPLESIZE * 0.434));
     free(resultChar);
 }
 END_TEST
