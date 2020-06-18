981bb7034c7ff70197aa3099aa4a8b34e24a46e4
Hannes Neuschmidt <8neuschm@informatik.uni-hamburg.de>
2020-06-18 20:41:52 +0200

    Issue #21; fixed missing exit condition in absolute_frequencies

--- Extraction/AbsoluteData.c
+++ Extraction/AbsoluteData.c
@@ -1,13 +1,11 @@
 #include "AbsoluteData.h"
 #include <stdlib.h>
 
-
-#define TYPE unsigned int
-
 TYPE max(TYPE a, TYPE b) { return a > b ? a : b; }
 
+// TODO add check if array is NULL or empty
 TYPE array_max_value(TYPE *array, size_t n_elems) {
-  TYPE max_value;
+  TYPE max_value = 0;
   for (TYPE i = 0; i < n_elems; i++) {
     max_value = max(max_value, array[i]);
   }
@@ -18,8 +16,12 @@ TYPE array_max_value(TYPE *array, size_t n_elems) {
    with number of infected people per simulation
 
    get: n_simulations: size_t length of
-   trial_results get: target: TYPE array
-   with length trial_results get: min, max:
+   trial_results
+  get: target: TYPE array
+   with length trial_results
+
+   // TODO
+   get: min, max:
    TYPE pointers to save minimum and
    maximum number of infected people
 
@@ -31,14 +33,14 @@ TYPE array_max_value(TYPE *array, size_t n_elems) {
 // TODO useful trimming could be applied here
 // so the returned array is not full of zeros
 void absolute_frequencies(TYPE *trial_results, size_t n_simulations,
-                          TYPE *target, size_t target_length
+                          TYPE *target//, size_t target_length
                           /* TYPE *min, */
                           /* TYPE *max */
     )
 {
-    size_t max_infected = target_length;
+    /* size_t max_infected = target_length; */
     TYPE n_infected;
-    for (TYPE i = 0; n_simulations; i++) {
+    for (TYPE i = 0; i < n_simulations; i++) {
         n_infected = trial_results[i];
         target[n_infected]++;
     }
--- Extraction/AbsoluteData.h
+++ Extraction/AbsoluteData.h
@@ -1,9 +1,13 @@
 #ifndef ABSOLUTE_DATA_H
 #define ABSOLUTE_DATA_H
 
+#include <stddef.h>
+
 #define TYPE unsigned int
 void absolute_frequencies(TYPE *trial_results, size_t n_simulations,
-                          TYPE *target, size_t target_length);
+                          TYPE *target
+                          // , size_t target_length
+                          );
 
 TYPE array_max_value(TYPE *array, size_t n_elems);
 #endif
