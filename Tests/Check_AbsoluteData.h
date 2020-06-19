36a2e4e94f66ee16c548008a8fafaffe89916e9e
Hannes Neuschmidt <8neuschm@informatik.uni-hamburg.de>
2020-06-19 17:33:58 +0200

    Issue #21; added test for absolute_frequencies

--- Tests/Check_AbsoluteData.h
+++ Tests/Check_AbsoluteData.h
@@ -1,12 +1,11 @@
 #include "../Extraction/AbsoluteData.h"
-#include <stdio.h>
-#include <check.h>
 
 START_TEST(test_array_max_value) {
     unsigned int temp, calculated_max, max = 100000;
     unsigned int array[] = {1, 2, 3, 5, 7, 11, 120, 2000, 100000};
     size_t len = sizeof array / sizeof array[0];
     for (unsigned int i = 0; i < len; i++) {
+      fprintf(stderr, "got here! %d\n", i);
         temp = array[len - 1];
         for (unsigned int j = len - 1; j > 0; j--) {
             array[j] = array[j - 1];
