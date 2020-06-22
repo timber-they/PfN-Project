5bf0fd3c57330aa8c23d668ec63126e79615a704
Hannes Neuschmidt <8neuschm@informatik.uni-hamburg.de>
2020-06-22 16:41:00 +0200

    Issue #21; added basic implementation for RelativeData

--- Extraction/RelativeData.c
+++ Extraction/RelativeData.c
@@ -1,5 +1,5 @@
 #include "RelativeData.h"
-#include <stdio.h> // TODO delete
+#include <assert.h>
 /*
   INFORMATION:
   * number of trials that returned n infected for n in [0, ..., max_infected]
@@ -12,21 +12,27 @@
 // ...)
 // Wie viele Durchläufe haben etwas im Berich von [Bucket] zurückgegeben?
 // Welchem Anteil der Versuche entspricht das?
-// TODO GGF config struct
-
+// TODO input checks
+// TODO step doesn't need to be calculated in every iteration
+// TODO ggf eine Funktion im Interface
 void bucket_indices(size_t target[], size_t population_size, size_t n_buckets) {
-    printf("%p\n", (void *) target);
-    printf("%zu\n", population_size);
-    printf("%zu\n", n_buckets);
+    for (size_t i = 0; i < n_buckets; ++i) {
+        target[i] = (size_t) ((population_size  + 1)/ n_buckets) * i;
+        }
 }
 
+// TODO target must be 0 initialized!
 void percentage_of_trials_in_bucket(double target[], size_t *buckets,
                                     size_t n_buckets,
                                     TYPE *occurences_of_n_infected,
                                     size_t population_size) {
-  printf("%p\n", (void *)target);
-  printf("%p\n", (void *)buckets);
-  printf("%zu\n", n_buckets);
-  printf("%p\n", (void *)occurences_of_n_infected);
-  printf("%zu\n", population_size);
+
+    for (size_t i = 0, bucket = 0; i <= population_size; ++i) {
+        // Relies on short circuiting!
+        if (bucket + 1 < n_buckets && i >= buckets[bucket+1])
+        {
+            bucket++;
+        }
+        target[bucket] += (double) occurences_of_n_infected[i] / population_size;
+    }
 } // Kann ggf bucket_beginnings selbst aufrufen
