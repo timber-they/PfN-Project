b6e5b5370eea8590522cc23b5498490f12ff2144
Julius Bittner <8bittner@informatik.uni-hamburg.de>
2020-06-23 15:07:32 +0200

    Removed GaussConf

--- Extraction/ConfidenceIntervals.c
+++ Extraction/ConfidenceIntervals.c
@@ -8,34 +8,6 @@
 
 #include "ConfidenceIntervals.h"
 
-// gaussConf: calculate confidence interval of some data with given standard deviation, number of values and level assuming gauss distribution
-// give: standard deviation
-// give: number of values as unsigned int
-// give: confidence level to use. Choose from 0.8, 0.85, 0.9, 0.95, 0.99, 0.995, 0.999
-// return: confidence as deviation from medium, to the upside as well as to the downside!
-double gaussConf(double standDev, unsigned int numOfVals, float level)
-{
-    float Z;
-    unsigned int Level = level * 1000;
-    //printf("%i\n", Level);
-    switch (Level) {
-	    case 800: Z = 1.282; break;
-	    case 850: Z = 1.440; break;
-	    case 900: Z = 1.645; break;
-	    case 950: Z = 1.960; break;
-            case 990: Z = 2.576; break;
-            case 995: Z = 2.807; break;
-	    case 999: Z = 3.291; break;
-	    default:
-		printf("Please choose level of confidence from 0.8, 0.85, 0.9, 0.95, 0.99, 0.995, 0.999");
-		return EXIT_FAILURE;
-    }
-
-    double s = sqrt(numOfVals);
-    double c = Z * standDev / s;
-    return c;
-}
-
 // conf_itvl: calculate confidence interval of given data with given level
 // give: data as sorted array of doubles
 // give: desired confidence level in (0.0, 1.0]
