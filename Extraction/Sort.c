8aef521cee9d3ccc85397f53af3805e639868b47
Pascal Klinger <18pklinge@informatik.uni-hamburg.de>
2020-06-25 15:24:40 +0200

    relative values
    getting Median and Medium after calculation of relative values

--- Extraction/Sort.c
+++ Extraction/Sort.c
@@ -7,6 +7,15 @@ int comp_int(const void *a, const void *b)
     return *((int *)a) - *((int *)b);
 }
 
+int comp_double(const void *a, const void *b)
+{
+    return *((double *)a) - *((double *)b);
+}
+
 void sort_int(int *array, size_t n_elems) {
     qsort(array, n_elems, sizeof *array, comp_int);
+}
+
+void sort_double(double *array, size_t n_elems) {
+    qsort(array, n_elems, sizeof *array, comp_double);
 }
--- Extraction/Sort.h
+++ Extraction/Sort.h
@@ -3,4 +3,7 @@
 
 // sorts the given array of integers
 void sort_int(int *array, size_t n_elems);
+
+// sorts the given array of doubles
+void sort_double(double *array, size_t n_elems);
 #endif
