2b09255e6910bd12dd159343dee1857a8489b0c2
RalfSchmelcher <65372609+RalfSchmelcher@users.noreply.github.com>
2020-06-27 16:07:25 +0200

    Issue #13 - Applying specifity and selectivity;updated test
    Issue #13 - Applying specifity and selectivity;updated test for new signature

--- Tests/Check_correction.h
+++ Tests/Check_correction.h
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 START_TEST(test_correction)
 {
-//    double f1 = correction(0.9);
-//    double diff = f1 - 0.99;
-//    if (diff < 0)
-//        diff = -diff;
-//    ck_assert(diff < 0.0000001);
+    double f1 = correction(0.9,0.909,0.991);
+    double diff = f1 - 0.99;
+    if (diff < 0)
+        diff = -diff;
+    ck_assert(diff < 0.0000001);
 }
 END_TEST
