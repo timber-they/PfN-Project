126e40199cc4f23667579e53471ab327a029dd3a
RalfSchmelcher <ralf.schmelcher@freenet.de>
2020-06-30 03:17:01 +0200

    Issue_41 - Just in time result writing; writing should now be just in time

--- Sampling/Program.c
+++ Sampling/Program.c
@@ -4,63 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// TODO: Read just in time
-static unsigned int *getSampleSizes(unsigned int sampleNumber, unsigned int sampleSize,
-        FILE* resultFile, unsigned int maxSampleSize)
-{    
-    // allocate the needed Memoey for SampleSize
-    unsigned int *sampleSizes = calloc(sampleNumber, sizeof(*sampleSizes));
-    if (sampleSize == -1)
-    {
-        unsigned int line = 0;
-        // Give Status
-        printf("Reading sample sizes from file\n");
-        // Check for existing file
-        if(resultFile == NULL)
-        {
-            fprintf(stderr, 
-                    "Couldn't open file for and no sample size was passed\n");
-            return NULL;
-        }
-        // Go though the file line by line
-        for (;fscanf(resultFile, "%u\n", &sampleSizes[line]) != EOF; line++)
-        {
-            // If a sampleSize is 0 or the SampleSize is larger than the maximum sample size give Error
-            if (sampleSizes[line] == 0 || sampleSizes[line] > maxSampleSize)
-            {
-                fprintf(stderr, 
-                        "Invalid sample size: %u; maximum sample size is: %u\n",
-                        sampleSizes[line], maxSampleSize);
-                return NULL;
-            }
-        }
-        // Line has to be = sampleNumber, if not give Error and close file
-        if (line != sampleNumber)
-        {
-            fprintf(stderr, "Received %u instead of %u sample sizes\n", 
-                    line, sampleNumber);
-            fclose(resultFile);
-            return NULL;
-        } else
-        {
-            // if Line = SampleNumber give Status
-            printf("Succesfully read %u sample sizes\n", sampleNumber);
-        }
-    }
-    else
-    {
-        // If SampleSize is not -1
-        printf("Using %u fixed sample sizes\n", sampleNumber);
-        // Write fixed sample sizes into array
-        for (unsigned int i = 0; i < sampleNumber; i++)
-        {
-            sampleSizes[i] = sampleSize;
-        }
-    }
-    // return the SampleSize
-    return sampleSizes;
-}
-
 // Simulation
 static int simulateTest(LazySource *source, double sensitivity, double specificity,
         int positives)
@@ -82,6 +25,7 @@ static int simulateTest(LazySource *source, double sensitivity, double specifici
 
     return positives;
 }
+
 // Method to check if File is already open and open it
 static FILE *openOrReopen(const char* filename, FILE *file)
 {
@@ -97,11 +41,9 @@ static FILE *openOrReopen(const char* filename, FILE *file)
 }
 
 // Simulate the Samples based on everything
-static void simulateSample(unsigned int *sampleSizes, unsigned int sample,
-        LazySource *source, double sensitivity, double specificity,
-        FILE *resultFile)
+static unsigned int simulateSample(unsigned int sampleSize, LazySource *source,
+        double sensitivity, double specificity)
 {
-    unsigned int sampleSize = sampleSizes[sample];
     unsigned int positives = 0;
     double correctedPercentage;
     unsigned int correctedPositives;
@@ -116,25 +58,49 @@ static void simulateSample(unsigned int *sampleSizes, unsigned int sample,
     correctedPercentage = correction((double) positives /
             sampleSize, sensitivity, specificity);
     correctedPositives = correctedPercentage * sampleSize;
+    return correctedPositives;
+}
 
-    printf("%uth sample result: %u\t%u\n", sample, sampleSize,
-            correctedPositives);
-    fprintf(resultFile, "%u\t%u\n", sampleSize, correctedPositives);
+// Simulate the Samples based on everything
+static unsigned int getSampleSize(unsigned int line, FILE* resultFile,
+                                  unsigned int maxSampleSize)
+{
+    unsigned int sampleSize = 0,
+                 index,
+                 exitCode = maxSampleSize + 1;
+    
+    // Check for existing file
+    if(resultFile == NULL)
+    {
+        fprintf(stderr, 
+                "Couldn't open file for and no sample size was passed\n");
+        return exitCode;
+    }
+    // Go though the file line by line
+    for (index = 0; fscanf(resultFile, "%u\n", &sampleSize) != EOF; index++)
+    {
+        if (index == line)
+        {
+            return sampleSize;
+        }
+    }
+    return exitCode;
 }
 
 // Everything together 
 int main(int argc, char *argv[])
 {
     // All the Variables we need
-    double p = 0;
-    unsigned int populationNumber = 0;
-    unsigned int sampleNumber = 0; // number of samples to create
-    unsigned int sampleSize = -1;
-    int seed = 96661;
-    LazySource *source;
-    double sensitivity = 0.909;
-    double specificity = 0.991;
-    unsigned int *sampleSizes;
+    double       p = 0,
+                 sensitivity = 0.909,
+                 specificity = 0.991;
+    unsigned int populationNumber = 0,
+                 sampleNumber = 0, // number of samples to create
+                 sampleSize = -1,
+                 line = 0,
+                 correctedPositives = 0;
+    int          seed = 96661;
+    LazySource  *source;
 
     // The output file    
     const char *resultFileName = "sampleResult.tsv";
@@ -147,29 +113,22 @@ int main(int argc, char *argv[])
                  || (argc > 4 && sscanf(argv[4], "%ud", &sampleSize) != 1)
                  || (argc > 5 && sscanf(argv[5], "%d", &seed) != 1)
                  || (argc > 6 && sscanf(argv[6], "%lf", &sensitivity) != 1)
-                 || (argc > 7 && sscanf(argv[7], "%lf", &specificity)!= 1))
+                 || (argc > 7 && sscanf(argv[7], "%lf", &specificity) != 1))
     {
         // If wrong input give correct Usage as Output and end
-        fprintf(stderr, "Usage: %s <p> <N> <n> [<S>] [<s>] [<se>] [<sp>]\n", argv[0]);
+        fprintf(stderr, "Usage: %s <p> <N> <n> [<S>] [<s>] [<se>] [<sp>]\n",
+                argv[0]);
         return 1;
     }
 
     // Open our resultFile
     resultFile = fopen(resultFileName, "r+");
-    sampleSizes = getSampleSizes(sampleNumber, sampleSize, resultFile,
-            populationNumber);
-    // Test for Error
-    if (sampleSizes == NULL)
-    {
-        return 1;
-    }
     // Test if our probabilty is beween 1 and 0
     if (p < 0 || p > 1)
     {
         fprintf(stderr, "Probability can't be less than 0 or more than 1\n");
         return 1;
-    }   
-    
+    }
 
     // Sampling_Init
     initRandom(seed);
@@ -185,10 +144,51 @@ int main(int argc, char *argv[])
     // create sampling for every sample-size
     source = getLazySource(populationNumber, p);
     printf("Got lazy source\n");
-    for(int sample = 0; sample < sampleNumber; sample++)
+    
+    if (sampleSize == -1)
     {
-        simulateSample(sampleSizes, sample, source, sensitivity, specificity,
-                resultFile);
+        // read sampleSizes from file
+        for(line = 0; line < sampleNumber; line++)
+        {
+            sampleSize = getSampleSize(line, resultFile, populationNumber);
+            // Test for Error
+            if (sampleSize > populationNumber)
+            {
+                fprintf(stderr, "File could not be opened or "
+                                "SampleSizes in file are not valid.\n");
+                return 1;
+            }
+            correctedPositives = simulateSample(sampleSize, source, sensitivity,
+                                                specificity);
+            printf("%uth sample result: %u\t%u\n", line, sampleSize,
+                   correctedPositives);
+            fprintf(resultFile, "%u\t%u\n", sampleSize, correctedPositives);
+        }
+        // Line has to be = sampleNumber, if not give Error and close file
+        if (line != sampleNumber)
+        {
+            fprintf(stderr, "Received %u instead of %u sample sizes\n", 
+                    line, sampleNumber);
+            return 1;
+        }
+        else
+        {
+            // if Line = SampleNumber give Status
+            printf("Succesfully read %u sample sizes\n", sampleNumber);
+        }
+    }
+    else
+    {
+        // read sampleSize from input
+        for(line = 0; line < sampleNumber; line++)
+        {
+            printf("Using %u fixed sample sizes\n", sampleNumber);
+            correctedPositives = simulateSample(sampleSize, source, sensitivity,
+                                                specificity);
+            printf("%uth sample result: %u\t%u\n", line, sampleSize,
+                   correctedPositives);
+            fprintf(resultFile, "%u\t%u\n", sampleSize, correctedPositives);
+        }
     }
 
     fclose(resultFile);
@@ -196,3 +196,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
